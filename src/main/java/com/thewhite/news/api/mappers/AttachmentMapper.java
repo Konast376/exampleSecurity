1f2863081488576cd23a49da65062c70beae2373
Sergey <00serd00@gmail.com>
2018-05-22 10:07:46 +1000

    еще тесты

--- src/main/java/com/thewhite/news/api/mappers/AttachmentMapper.java
+++ src/main/java/com/thewhite/news/api/mappers/AttachmentMapper.java
@@ -4,6 +4,8 @@ import com.thewhite.news.api.dto.AttachmentDTO;
 import com.thewhite.news.model.Attachment;
 import com.whitesoft.api.mappers.BaseMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 /**
  * Created by Vdovin S. on 17.05.18.
@@ -14,4 +16,7 @@ import org.mapstruct.Mapper;
  */
 @Mapper
 public interface AttachmentMapper extends BaseMapper<Attachment, AttachmentDTO> {
+    @Override
+    @Mappings(@Mapping(target = "newsRecordId", source = "newsRecord.id"))
+    AttachmentDTO toDTO(Attachment attachment);
 }
--- src/test/java/com/thewhite/news/api/NewsRecordControllerIT.java
+++ src/test/java/com/thewhite/news/api/NewsRecordControllerIT.java
@@ -2,6 +2,7 @@ package com.thewhite.news.api;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.thewhite.news.api.dto.AttachmentDTO;
 import com.thewhite.news.api.dto.NewsCreateDTO;
 import com.thewhite.news.api.dto.NewsRecordDTO;
 import com.thewhite.news.api.dto.NewsUpdateDTO;
@@ -13,16 +14,24 @@ import com.thewhite.utils.BaseIT;
 import com.whitesoft.api.dto.CollectionDTO;
 import com.whitesoft.util.test.CustomAssertion;
 import com.whitesoft.util.test.MvcRequester;
+import org.hamcrest.core.IsNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.RequestResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.MimeType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Date;
 import java.util.List;
@@ -32,7 +41,10 @@ import static com.thewhite.news.model.RecordStatus.PROJECT;
 import static com.thewhite.news.model.RecordStatus.PUBLISHED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,10 +63,13 @@ public class NewsRecordControllerIT extends BaseIT {
     @Autowired
     private RestTemplate restTemplate;
 
-    private MockRestServiceServer mockService;
+    private MockRestServiceServer mockServer;
 
     @Autowired
     private NewsRecordRepository newsRecordRepository;
+
+    @Value("${content.storage.url}")
+    private String contentStorageUrl;
 
     @MockBean
     private AuthService authService;
@@ -66,7 +81,7 @@ public class NewsRecordControllerIT extends BaseIT {
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).alwaysDo(print()).build();
-        mockService = MockRestServiceServer.createServer(restTemplate);
+        mockServer = MockRestServiceServer.createServer(restTemplate);
         when(authService.getCurrentUserId()).thenReturn(testUserId);
     }
 
@@ -327,20 +342,38 @@ public class NewsRecordControllerIT extends BaseIT {
     }
 
     /**
-     * Тест
+     * Тест загрузки вложений
      *
      * @throws Exception
      */
     @Test
     public void attach() throws Exception {
         //Arrange
-        //Act
+        String content = "content";
+        mockServer.expect(requestTo(UriComponentsBuilder.fromUriString("{url}/create")
+                                                        .buildAndExpand(contentStorageUrl)
+                                                        .toUri()))
+                  .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+                  .andRespond(withSuccess("{\"nodeId\":\"f520dad3-c052-4365-a767-542973f3d4b1\"," +
+                                          "\"mediaType\":\"application/pdf\"}", MediaType.valueOf("application/json")));
 
-        //Assert
+        //Act
+        AttachmentDTO result = MvcRequester.on(mockMvc)
+                                           .to("{url}/{id}/attachments/create", NewsRecordController.URI, newsRecordId)
+                                           .withFile("data", "test.pdf", MimeType.valueOf("application/pdf"), content.getBytes())
+                                           .upload()
+                                           //Assert
+                                           .doExpect(status().isOk())
+                                           .returnAs(AttachmentDTO.class);
+        CustomAssertion.assertThat(result)
+                       .lazyMatch(AttachmentDTO::getId, IsNull.notNullValue())
+                       .lazyCheck(AttachmentDTO::getName, "test.pdf")
+                       .lazyCheck(AttachmentDTO::getNewsRecordId, newsRecordId)
+                       .check();
     }
 
     /**
-     * Тест
+     * Тест получения списка вложений
      *
      * @throws Exception
      */
@@ -348,8 +381,18 @@ public class NewsRecordControllerIT extends BaseIT {
     public void getAttachments() throws Exception {
         //Arrange
         //Act
-
+        CollectionDTO<AttachmentDTO> results = MvcRequester.on(mockMvc)
+                                                           .to("{url}/{id}/attachments/list", NewsRecordController.URI, newsRecordId)
+                                                           .get()
+                                                           .doExpect(status().isOk())
+                                                           .doReturn(new TypeReference<CollectionDTO<AttachmentDTO>>() {});
         //Assert
+        assertThat(results.getItems()).extracting(AttachmentDTO::getId,
+                                                  AttachmentDTO::getName,
+                                                  AttachmentDTO::getNewsRecordId)
+                                      .containsExactly(tuple(UUID.fromString("a0772192-3df1-419a-87ae-d824ae911e4e"),
+                                                             "name1.pdf",
+                                                             UUID.fromString("84885588-33fe-49fb-a3e7-097d790eaa00")));
     }
 
 }
